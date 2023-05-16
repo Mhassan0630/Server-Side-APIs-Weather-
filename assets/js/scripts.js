a22b611caf9ff27d55ad237f6f37eb0efd4c51b7
MUNTAAS <98139114+Mhassan0630@users.noreply.github.com>
2023-05-16 06:25:55 -0500

    added the handlesearch

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,3 +2,19 @@ var key = '66ab4b644320689237397dc493746b1d';
 var city = "Minneapolis"
 var date = moment().format('dddd, MMMM Do YYYY');
 var cityHist = [];
+
+function handleSearch(event) {
+    event.preventDefault();
+    let newCity = $('.textVal').val().trim();
+    if (newCity !== "") {
+        city = newCity;
+        if (!cityHist.includes(city)) {
+            cityHist.push(city);
+        }
+        localStorage.setItem('city', JSON.stringify(cityHist));
+        updateCityHistory();
+        $('.textVal').val('');
+        getWeatherToday();
+        getFiveDayForecast();
+    }
+}
